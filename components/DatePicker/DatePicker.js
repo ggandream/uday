3f44b2219644cb240880a1b33359677b60411d5a
Andrea <ggandream24@gmail.com>
2026-08-01 21:17:13 -0600

    hotfix: datepicker maxdate

--- components/DatePicker/DatePicker.js
+++ components/DatePicker/DatePicker.js
@@ -1,6 +1,18 @@
 import { icons } from "../../icons/icon.js";
 
 export const DatePicker = () => {
+  const date = new Date().getDate();
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+
+  const maxDate = String(
+    year +
+      "-" +
+      String(month).padStart(2, "0") +
+      "-" +
+      String(date).padStart(2, "0"),
+  );
+
   return `<div class="date">
                 <button class="date__btn" type="button" aria-label="Choose a date">
                   <span><span class="date__icon" aria-hidden="true" focusable="false">${icons["calendar"] ?? ""}</span></span>
@@ -10,7 +22,7 @@ export const DatePicker = () => {
                     class="date__input"
                     name="date__input"
                     min="1995-06-20"
-                    max="${new Date().toISOString().split("T")[0]}"
+                    max="${maxDate}"
                 />
           </div>`;
 };
